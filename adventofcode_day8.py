d740248191acc9cefa28a73984bde01b73d273b7
Ynadroid <danyberlemont@free.fr>
2015-12-14 10:56:37 +0100

    Day 8 part 1 OK

--- adventofcode_day8.py
+++ adventofcode_day8.py
@@ -1,6 +1,7 @@
 # Advent of Code Day 8
 
 def countCharacters(string):
+	#print('----- counting',string)
 	pos = 0
 	count = 0
 	inString = False
@@ -9,46 +10,59 @@ def countCharacters(string):
 	while pos < len(string):
 		c = string[pos]
 		if c == '"' and not inString:
+			#print('Entering in string')
 			inString = True
 			pos += 1
 			continue
 		if inString:
 			if hexaCharacter:
+				#print('Hexa not counted', pos, pos+2)
 				pos += 2
+				count += 1
 				hexaCharacter = False
+				oneBackSlash = False
 				continue
 			
-			if c == '\\':
+			if c == '\\' and not oneBackSlash:
+				#print('Backslash counted')
 				oneBackSlash = True
 				pos += 1
 				continue
 			elif oneBackSlash and c == 'x':
+				#print('Hexa detected')
 				hexaCharacter = True
 				pos += 1
 				continue
 				
 			if oneBackSlash:
+				#print('Counting backslash',c)
 				count += 1
 				pos += 1
 				oneBackSlash = False
 				continue
+			elif c == '"':
+				#print('End of string')
+				pos += 1
+				continue
 			
+			#print('Normal counting',c,pos,count)
 			count += 1
 			pos += 1
-	print(count)
-	return count-1
+	#print(count)
+	return count
 
-with open('adventofcode_day8_test_input.txt') as f:
-#with open('adventofcode_day8_input.txt') as f:
+# with open('adventofcode_day8_test_input.txt') as f:
+with open('adventofcode_day8_input.txt') as f:
 	input = [line.strip() for line in f]
 
 charofcodecount = 0
 charcount = 0
 
 for string in input:
-	print(string)
+	print(string, ',', len(string), ',', countCharacters(string))
 	charofcodecount += len(string)
 	charcount += countCharacters(string)
 
 print("Total characters of code count:",charofcodecount)
 print("Total characters count:",charcount)
+print("Problem part 1 answer:",charofcodecount-charcount)
